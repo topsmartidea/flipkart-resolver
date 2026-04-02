313ea3bfef69ca3c4721da7ce846539662d791c2
topsmartidea <91816797+topsmartidea@users.noreply.github.com>
2026-04-02 23:08:43 +0530

    Update js_resolver.js

--- js_resolver.js
+++ js_resolver.js
@@ -9,14 +9,14 @@ let browser;
 (async () => {
   try {
     browser = await puppeteer.launch({
-      headless: true,
-      args: [
-        "--no-sandbox",
-        "--disable-setuid-sandbox",
-        "--disable-dev-shm-usage",
-        "--disable-gpu"
-      ]
-    });
+    headless: true,
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-gpu"
+    ]
+  });
 
     console.log("🚀 Browser Ready");
 
